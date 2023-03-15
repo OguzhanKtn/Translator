6f313436f66a3c794cc113f44cebaad35686f488
OguzhanKtn <oguzhanktn@hotmail.com>
2023-03-15 20:58:23 +0300

    project-completed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const toLang = document.querySelector("#to-lang");
 const btnTranslate = document.querySelector("#btnTranslate");
 const fromText = document.querySelector("#from-text");
 const toText = document.querySelector("#to-text");
+const exchange = document.querySelector(".exchange");
+const icons = document.querySelectorAll(".icons");
 
 for(let lang in languages){
      let option = `<option value="${lang}">${languages[lang]}</option>`;
@@ -22,4 +24,37 @@ btnTranslate.addEventListener("click",()=>{
           toText.value = data.responseData.translatedText;
      })
 });
+
+exchange.addEventListener("click",() => {
+     let text = fromText.value;
+     fromText.value = toText.value;
+     toText.value = text;
+
+     let lang = fromLang.value;
+     fromLang.value = toLang.value;
+     toLang.value = lang;
+
+});
+
+for(let icon of icons){
+     icon.addEventListener("click",(element)=>{
+          if(element.target.classList.contains("fa-copy")){
+               if(element.target.id =="from"){
+                    navigator.clipboard.writeText(fromText.value);
+               }else{
+                    navigator.clipboard.writeText(toText.value);
+               }
+          }else{
+               let utterance;
+               if(element.target.id =="from"){
+                    utterance = new SpeechSynthesisUtterance(fromText.value);
+                    utterance.lang = fromLang.value;
+               }else{
+                    utterance = new SpeechSynthesisUtterance(toText.value);
+                    utterance.lang = toLang.value;
+               }
+               speechSynthesis.speak(utterance);
+          }
+     })
+}
 
